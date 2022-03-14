257f1ddd5e2cc261fc4b1c30959a6bbb9352f14c
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 10:35:44 -0700

    adds index, show, create, update, and destroy routes & methods

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,41 @@
+class SongsController < ApplicationController
+
+  def index
+    song = Song.all
+    render json: song.as_json
+  end
+  
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    song.save
+    render json: song.as_json
+  end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+
+    song.save
+    render json: song.as_json
+  end
+
+  def destroy
+    song = Song.find_by(id: params[:id])
+    song.destroy
+    render json: {message: "The song was deleted from your library!"}
+  end
+
+end
